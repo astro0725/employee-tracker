a9d5833296eddcc3dec8dcfe1162f8cecd6ac4ce
astro <angelica.strong0725@gmail.com>
2024-02-12 15:12:24 -0800

    added title

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// initialize queries & tables
+db = require('./db/connection'); 
+const appTitle = './lib/title.js';
+const options = require('./lib/queries');
+
+// Function to start the application
+function startApp() {
+    console.log(appTitle);
+    options(); // Call the options function to display the main menu
+}
+
+db.promise().connect()
+    .then(() => {
+        console.log('Connected to the database.');
+        startApp();
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err);
+    });
